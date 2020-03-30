e9d6261e42cd7b5399d3d649fd8ba1fad9eb4df6
asamant <aniket.a.samant@outlook.com>
2020-03-30 03:12:22 +0200

    Final version of the script for now. Also uploading initial screenshots

--- script.py
+++ script.py
@@ -33,6 +33,55 @@ def enable_traffic_updates(driver):
     xpath_traffic_button = """//*[@id="settings"]/div/div[2]/div/ul[2]/li[2]/button"""
     driver.find_element_by_xpath(xpath_traffic_button).click()
 
+# Take screenshots for cities
+def generate_city_screenshot(city, driver, screenshot_filename):
+    
+        # search for city and navigate there
+        search_box = driver.find_element_by_name('q')
+        search_box.send_keys(city + Keys.ENTER)
+        time.sleep(3)
+
+        # Clear search button
+        clear_search_button = driver.find_element_by_class_name('sbcb_a')
+        clear_search_button.click()
+        time.sleep(2)
+        
+        # take screenshot
+        driver.save_screenshot(screenshot_filename)
+        time.sleep(2)
+
+
+def switch_to_typical_traffic(driver):
+
+    # click on the "Live/Typical traffic" dropdown menu button
+    traffice_dropdown_xpath = "/html/body/jsl/div[3]/div[9]/div[23]/div[1]/div[1]/div[3]/div/div/div/span/span[1]/div/div/div/div[1]"
+    traffic_dropdown = driver.find_element_by_xpath(traffice_dropdown_xpath)
+    traffic_dropdown.click()
+    time.sleep(2)
+
+    # switch to "Typical traffic" from "Live traffic". assumes that traffic is enabled already, and "Live traffic" is already selected
+    ActionChains(driver).send_keys(Keys.DOWN).send_keys(Keys.ENTER).perform()
+    time.sleep(2)
+
+    # horrible hack - click on slider and keep moving until we hit 6:30-ish PM
+    traffic_time_slider = driver.find_element_by_class_name("widget-layer-slider-thumb")
+    traffic_time_slider.click()
+    traffic_time_width = driver.find_element_by_class_name("widget-layer-slider-enabled")
+    width_regex = re.compile('width: ([0-9]+)%')
+    
+    current_slider_pos = 15
+
+    # 76% on slider = approximately 6:30 PM. Peak traffic time
+    while (current_slider_pos < 76):
+        style_string = traffic_time_width.get_attribute("style")
+        matchObj = width_regex.match(style_string)
+        if matchObj:
+            current_slider_pos = int(matchObj.group(1)) # get current slider position
+        ActionChains(driver).send_keys(Keys.RIGHT).perform() # move slider to the right
+    
+    time.sleep(2) # not needed per se
+
+
 def main():
     try:
         # https://selenium-python.readthedocs.io/api.html#module-selenium.webdriver.chrome.webdriver
@@ -53,70 +102,28 @@ def main():
 
     # city list
     # https://www.worldatlas.com/articles/cities-with-the-worst-traffic-in-the-world.html
-    # cities = ["New York", "Beijing", "Tainan", "Rio de Janeiro", "Chengdu", "Istanbul", "Bucharest", "Chongqing", "Jakarta", "Bangkok", "Mexico City", "Mumbai", "Bangalore"]
-    
-    cities = ["New York"] 
+    cities = ["New York", "Beijing", "Tainan", "Rio de Janeiro", "Chengdu", "Istanbul", "Bucharest", "Chongqing", "Jakarta", "Bangkok", "Mexico City", "Mumbai", "Bangalore"]
 
     # access the Google Maps page
     driver.get('http://maps.google.com')
-    time.sleep(5)
-
+    time.sleep(3)
+    
+    # assumes these updates are "Live traffic" updates by default
     enable_traffic_updates(driver)
     time.sleep(2)
 
-    search_box = driver.find_element_by_name('q')
-
     for city in cities:
-        search_box.send_keys(city + Keys.ENTER)
-        time.sleep(3)
-
-        # Clear search button
-        clear_search_button = driver.find_element_by_class_name('sbcb_a')
-        clear_search_button.click()
-        time.sleep(2)
-
         city_name = city[:].replace(" ", "-") # copy to a new string first
-        driver.save_screenshot(screenshot_path + '/' + city_name + '-corona.png')
-        time.sleep(2)
+        screenshot_filename = screenshot_path + '/' + city_name + '-corona.png'
+        generate_city_screenshot(city, driver, screenshot_filename)
 
-    traffice_dropdown_xpath = "/html/body/jsl/div[3]/div[9]/div[23]/div[1]/div[1]/div[3]/div/div/div/span/span[1]/div/div/div/div[1]"
-    traffic_dropdown = driver.find_element_by_xpath(traffice_dropdown_xpath)
-    traffic_dropdown.click()
-    time.sleep(2)
-
-    # switch to "Typical traffic" from "Live traffic". assumes that traffic is enabled already, and "Live traffic" is already selected
-    ActionChains(driver).send_keys(Keys.DOWN).send_keys(Keys.ENTER).perform()
-    time.sleep(2)
-
-    traffic_time_slider = driver.find_element_by_class_name("widget-layer-slider-thumb")
-    traffic_time_slider.click()
-
-    traffic_time_width = driver.find_element_by_class_name("widget-layer-slider-enabled")
-    width_regex = re.compile('width: ([0-9]+)%')
-    
-    current_slider_pos = 15
-
-    # 76% on slider = approximately 6:30 PM. Peak traffic time
-    while (current_slider_pos < 76):
-        style_string = traffic_time_width.get_attribute("style")
-        matchObj = width_regex.match(style_string)
-        if matchObj:
-            current_slider_pos = int(matchObj.group(1)) # get current slider position
-        ActionChains(driver).send_keys(Keys.RIGHT).perform() # move slider to the right until we reach 6:30 PM
-
+    # to compare with the previous screenshots
+    switch_to_typical_traffic(driver)
     
     for city in cities:
-        search_box.send_keys(city + Keys.ENTER)
-        time.sleep(3)
-
-        # Clear search button
-        clear_search_button = driver.find_element_by_class_name('sbcb_a')
-        clear_search_button.click()
-        time.sleep(2)
-
         city_name = city[:].replace(" ", "-") # copy to a new string first
-        driver.save_screenshot(screenshot_path + '/' + city_name + '-typical.png')
-        time.sleep(2)
+        screenshot_filename = screenshot_path + '/' + city_name + '-typical.png'
+        generate_city_screenshot(city, driver, screenshot_filename)
 
     driver.quit()
 
